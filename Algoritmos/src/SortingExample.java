bdceecc1256154d75ddc94d8ae92e5bd3aaeccd5
nechks <yurimartinelli123@gmail.com>
2024-08-24 16:51:57 -0300

    alteracao feita com sucesso.

--- Algoritmos/src/SortingExample.java
+++ Algoritmos/src/SortingExample.java
@@ -5,15 +5,17 @@ import OrdenacaoAleatoria.OrdenacaoAleatoria;
 import OrdenacaoCrescente.OrdenacaoCrescente;
 import OrdenacaoDecrescente.OrdenacaoDecrescente;
 
+
 public class SortingExample {
 
     static OrdenacaoCrescente ordenacaoCrescente = new OrdenacaoCrescente();
     static OrdenacaoDecrescente ordenacaoDecrescente = new OrdenacaoDecrescente();
     static OrdenacaoAleatoria ordenacaoAleatoria = new OrdenacaoAleatoria();
+    
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         while (true) {
             
         System.out.println("");
@@ -45,70 +47,86 @@ public class SortingExample {
             System.out.println("Erro ao ler o arquivo de entrada: " + e.getMessage());
         }
 
+        String algorithm = "";
         double tempoInicial = System.currentTimeMillis();
 
         switch (choice) {
             // Ordenação Crescente
             case 1:
                 ordenacaoCrescente.bubbleSort(numbers);
+                algorithm = "BubbleSort Crescente";
                 break;
             case 2:
                 ordenacaoCrescente.insertionSort(numbers);
+                algorithm = "Insertion Sort";
                 break;
             case 3:
                 ordenacaoCrescente.selectionSort(numbers);
+                algorithm = "SelectionSort Crescente";
                 break;
             case 4:
                 ordenacaoCrescente.mergeSort(numbers, 0, n - 1);
+                algorithm = "MergeSort Crescente";
                 break;
             case 5:
                 ordenacaoCrescente.quickSort(numbers, 0, n - 1);
+                algorithm = "QuickSort Crescente";
                 break;
             case 6:
                 ordenacaoCrescente.heapSort(numbers);
+                algorithm = "HeapSort Crescente";
                 break;
 
             // Ordenação Decrescente
             case 7:
                 ordenacaoDecrescente.bubbleSort(numbers);
+                algorithm = "BubbleSort Decrescente";
                 break;
             case 8:
                 ordenacaoDecrescente.insertionSort(numbers);
+                algorithm = "InsertionSort Decrescente";
                 break;
             case 9:
                 ordenacaoDecrescente.selectionSort(numbers);
+                algorithm = "SelectionSort Decrescente";
                 break;
             case 10:
                 ordenacaoDecrescente.mergeSort(numbers, 0, n - 1);
+                algorithm = "MergeSort Decrescente";
                 break;
             case 11:
                 ordenacaoDecrescente.quickSort(numbers, 0, n - 1);
+                algorithm = "QuickSort Decrescente";
                 break;
             case 12:
                 ordenacaoDecrescente.heapSort(numbers);
+                algorithm = "HeapSort Decrescente";
                 break;
 
             // Ordenação Aleatória (Embaralhamento)
             case 13:
                 ordenacaoAleatoria.bubbleShuffle(numbers);
+                algorithm = "BubbleShuffle Aleatório";
                 break;
             case 14:
                 ordenacaoAleatoria.insertionShuffle(numbers);
+                algorithm = "InsertionShuffle Aleatório";
                 break;
             case 15:
                 ordenacaoAleatoria.selectionShuffle(numbers);
+                algorithm = "SelectionShuffle Aleatório";
                 break;
             case 16:
                 ordenacaoAleatoria.mergeShuffle(numbers, 0, n - 1);
+                algorithm = "MergeShuffle Aleatório";
                 break;
             case 17:
                 ordenacaoAleatoria.quickShuffle(numbers, 0, n - 1);
+                algorithm = "QuickShuffle Aleatório";
                 break;
             case 18:
                 ordenacaoAleatoria.heapShuffle(numbers);
-                break;
-            case 19:
-                System.out.println("Aplicacao encerrada");
+                algorithm = "HeapShuffle Aleatório";
                 break;
 
             default:
@@ -130,10 +148,21 @@ public class SortingExample {
         }
 
         System.out.println("Ordenação concluída e salva no arquivo: " + outputFileName);
-        
+        exportarResultados(algorithm, n, tempoExecucao, "resultados.txt");
         
         
     }
         scanner.close();
     }
+
+    public static void exportarResultados(String algorithm, int numElements, double tempoExecucao, String outputFile) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile, true))) {
+            bw.write(String.format("%-25s %-20d %.4f", algorithm, numElements, tempoExecucao));
+            bw.newLine();
+            System.out.println("Resultados exportados para: " + outputFile);
+        } catch (IOException e) {
+            System.out.println("Erro ao exportar os resultados: " + e.getMessage());
+        }
+    }
+
 }
